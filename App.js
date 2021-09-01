1c6118c21134ca4645abb30cfd8e7c09105c52bc
TKNRFJK1208 <takanori.fujiki@gmail.com>
2021-08-31 21:33:35 -0700

    Car List components

--- App.js
+++ App.js
@@ -1,16 +1,11 @@
 import React from 'react';
-import { StyleSheet, Text, View, ImageBackground } from 'react-native';
-import CarItem from './components/CarItem';
+import { StyleSheet, View } from 'react-native';
+import CarsList from './components/CarsList';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <CarItem 
-        name={"Model X"}
-        tagline={"Order Online For"}
-        taglineCTA={"Touchless Delivery"}
-        image={require('./assets/images/Model3.jpeg')}
-      />
+      <CarsList />
     </View>
   );
 }
---
+++ components/CarsList/index.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import { View, Text, FlatList, Dimensions } from 'react-native';
+import CarItem from '../CarItem/index';
+
+import styles from './styles';
+import cars from './cars';
+
+const CarsList = (props) => {
+  return (
+    <View style={styles.container}>
+      <FlatList 
+        data={cars}
+        renderItem={({item}) => <CarItem car={item} />}
+        // Cancel the vertical scroll bar
+        showsVerticalScrollIndicator={false}
+        // when you scroll the screen, it will change to the next item smoothly
+        snapToAlignment={'start'}
+        decelerationRate={'fast'}
+        snapToInterval={Dimensions.get('window').height}
+      />
+    </View>
+  );
+};
+
+export default CarsList;
